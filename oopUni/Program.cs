09be33643cf751f75bfb5b9ffaf52f795cf9f3c8
MousaZa <90974200+MousaZa@users.noreply.github.com>
2024-02-23 16:22:40 +0300

    starting with classes

--- oopUni/Program.cs
+++ oopUni/Program.cs
@@ -3,29 +3,26 @@ using System;
 namespace oopUni
 
 {
-    public enum Gender
+    class MyFirstClass
     {
-        Male,
-        Female
+        public MyFirstClass(int value)
+        {
+            _mValue = value;
+        }
+        
+        int _mValue;
+        public void PrintValue()
+        {
+            Console.WriteLine("the value is " + _mValue);
+        }
     }
+
     internal class Program
     {
         public static void Main(string[] args)
         {
-            double IdealWeight(short height , short age ,Gender gender)
-            {
-                if (gender == Gender.Male)
-                {
-                    return (height - 100 + (double)age / 10) * 0.9;
-                }
-                else
-                {
-                    return (height - 100 + (double)age / 10) * 0.8;
-                }
-            }
-
-            Console.WriteLine(IdealWeight(187, 19, Gender.Male));
-            Console.WriteLine(IdealWeight(187, 19, Gender.Female));
+            MyFirstClass myFirstClass = new MyFirstClass(5);
+            myFirstClass.PrintValue();
         }
     }
 }
