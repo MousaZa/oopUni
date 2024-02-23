1801f2f864690698d23ded4e181d65a6bc6b9896
MousaZa <90974200+MousaZa@users.noreply.github.com>
2024-02-23 14:36:31 +0300

    forEach

--- oopUni/Program.cs
+++ oopUni/Program.cs
@@ -7,14 +7,11 @@ namespace oopUni
     {
         public static void Main(string[] args)
         {
-            int[][] jagged = new int[3][];
-            jagged[0] = new int[4] { 0, 1, 2, 3 };
-            jagged[1] = new int[2] { 4, 5 };
-            jagged[2] = new int[3] { 6, 7, 8 };
-            
-            // 0 1 2 3
-            // 4 5 
-            // 6 7 8
+            int[] arr = new int[5] { 1, 2, 3, 4, 5 };
+            foreach (int element in arr)
+            {
+                Console.WriteLine(element);
+            }
         }
     }
 }
