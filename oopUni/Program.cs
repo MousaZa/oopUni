a11c56cbf7e0ed46cec6027fa6c1e742b04a018d
MousaZa <90974200+MousaZa@users.noreply.github.com>
2024-02-23 14:33:49 +0300

    jagged arrays

--- oopUni/Program.cs
+++ oopUni/Program.cs
@@ -7,7 +7,14 @@ namespace oopUni
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome");
+            int[][] jagged = new int[3][];
+            jagged[0] = new int[4] { 0, 1, 2, 3 };
+            jagged[1] = new int[2] { 4, 5 };
+            jagged[2] = new int[3] { 6, 7, 8 };
+            
+            // 0 1 2 3
+            // 4 5 
+            // 6 7 8
         }
     }
 }
