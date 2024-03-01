f754fd15dd303ae9d023b57d534b3988dadc336d
MousaZa <90974200+MousaZa@users.noreply.github.com>
2024-03-01 14:37:09 +0300

    init (c# 9.0)

--- oopUni/Program.cs
+++ oopUni/Program.cs
@@ -5,7 +5,7 @@ namespace oopUni
 {
     class MyFirstClass
     {
-
+        // todo public int value {init; get;} = 0 ;
         private int mAge = 0;
         public int age
         {
@@ -19,6 +19,8 @@ namespace oopUni
     {
         public static void Main(string[] args)
         {
+            // todo MyFirstClass myFirstClass1 = new MyFirstClass(){value = 10} ;
+            // todo myFirstClass1.value = 20; // error
             MyFirstClass myFirstClass = new MyFirstClass();
             myFirstClass.age = 20;
             Console.WriteLine(myFirstClass.age);
