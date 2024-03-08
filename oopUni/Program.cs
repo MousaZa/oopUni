e5029c637fb15a8780da4d882d4ded1e595e8362
MousaZa <mous.zeydan@gmail.com>
2024-03-08 19:50:56 +0300

    "inheritance 2"

--- oopUni/Program.cs
+++ oopUni/Program.cs
@@ -5,12 +5,34 @@ namespace oopUni
 {
     class BaseClass
     {
-        public int bNumber = 10 ;
-
-        
-        public void PrintNumber()
+        public int bNumber { set; get; } = 13 ;
+        public BaseClass()
         {
-            Console.WriteLine(bNumber);
+        }
+
+        public BaseClass(int number)
+        {
+           if(number > 0) bNumber = number;
+           else Console.WriteLine("Number should be greater than 0");
+        }
+        public void printNumber()
+        {
+            Console.WriteLine("Your Number is : " + bNumber);
+        }
+        private bool isPrime()
+        {
+            if (bNumber == 1) return false;
+            if (bNumber == 2) return true;
+            for (int i = 2; i < bNumber; i++)
+            {
+                if (bNumber % i == 0) return false;
+            }
+            return true;
+        }
+
+        public void printIsPrime()
+        {
+            Console.WriteLine( isPrime() ? "Your Number is Prime" : "Your Number is not Prime");
         }
     }
     class DerivedClass : BaseClass
@@ -24,11 +46,11 @@ namespace oopUni
     }
 
     internal class Program
-    {
+    { 
         public static void Main(string[] args){
-            DerivedClass myFirstClass = new DerivedClass();
+            BaseClass myFirstClass = new BaseClass();
             Console.WriteLine(myFirstClass.bNumber);
-            Console.WriteLine(myFirstClass.dNumber);
+            myFirstClass.printIsPrime();
         }
     }
 }
