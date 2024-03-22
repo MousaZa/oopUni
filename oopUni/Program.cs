97fb7b6ad6ee6996dbecd57c6d1133d4f78f0cb7
MousaZa <mous.zeydan@gmail.com>
2024-03-22 15:29:36 +0300

    Casting

--- oopUni/Program.cs
+++ oopUni/Program.cs
@@ -3,49 +3,34 @@ using System;
 namespace oopUni
 
 {
-    class MyFirstClass
-    {
-        private int mAge = 0;
-
-        public MyFirstClass()
-        {
-            
-        }
-
-        public MyFirstClass(int _age)
-        {
-            mAge = _age;
-        }
-
-        public void setAge(int _age)
-        {
-            mAge = _age;
-        }
-
-        public void printAge()
-        {
-            Console.WriteLine("your age is " + mAge);
-        }
-
-        ~MyFirstClass()
-        {
-            Console.WriteLine("Object deleted !");
-        }
-
-        public int addYears(int years)
-        {
-            return mAge + years;
-        }
-    }
+    
 
     internal class Program
     {
-        public static void Main(string[] args){
-            MyFirstClass myFirstClass = new MyFirstClass(10);
-            myFirstClass.printAge();
-            myFirstClass.setAge(20) ;
-            myFirstClass.printAge();
-            Console.WriteLine(myFirstClass.addYears(5));
+        public static void Main(string[] args)
+        {
+            // Point2D point = new Point2D(3 , 4);
+            // point.ConvertToPolar();
+            Shape shape = new Shape(new Point2D(3,4));
+            // shape.DrawShape();
+            Square square = new Square(new Point2D(3, 4));
+            // square.DrawShape();
+            Circle circle = new Circle(new Point2D(3, 4), 5);
+            // circle.DrawShape();
+            Shape[] shapes =
+            {
+                shape, square, circle
+            };
+
+            foreach (var sh in shapes)
+            {
+                sh.DrawShape();
+                sh.PrintInformation();
+                
+                if (sh is Circle)
+                    ((Circle)sh).PrintArea();
+                Line.DrawLine('x' , 40);
+            }
         }
     }
 }
