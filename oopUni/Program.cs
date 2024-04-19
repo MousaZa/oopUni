b872fb24225cbbd7edc72fdf18e59066231f82a7
MousaZa <mous.zeydan@gmail.com>
2024-04-19 16:12:58 +0300

    Exeptions

--- oopUni/Program.cs
+++ oopUni/Program.cs
@@ -3,83 +3,68 @@
 namespace oopUni
 {
 
-    
-    internal class InfoClass {
-        public static void printInfo(Point p)
-        {
-            p.printCoordinates();
-        }
-    }
-    internal abstract class Point
+    internal class Operation
     {
-        public int _x { set; get; }
-        public int _y { set; get; }
+        public int Num1 { set; get; }
+        public int Num2 { set; get; }
+        public char OperationSign { set; get; }
 
-        public void printInfo()
+        public Operation()
         {
-            Console.WriteLine("This is a POINT!");
-        }
-
-        public abstract void printCoordinates();
-    }
-
-    internal class Point2D : Point
-    {
-        public Point2D()
-        {
-            _x = 0;
-            _y = 0;
-        }
-        public Point2D(int x , int y)
-        {
-            _x = x;
-            _y = y;
+            Num1 = 0;
+            Num2 = 0;
+            OperationSign = '+';
         }
 
-        public override void printCoordinates()
+        public Operation(int num1 , int num2 , char operationSign)
         {
-            Console.WriteLine("The coordinates of the point is ("+_x+","+_y+").");
-        }
-    }
-    
-    internal class Point3D : Point
-    {
-        public int _z { get; set; }
-        public Point3D()
-        {
-            _x = 0;
-            _y = 0;
-            _z = 0;
-        }
-        public Point3D(int x , int y,int z)
-        {
-            _x = x;
-            _y = y;
-            _z = z;
+            Num1 = num1;
+            Num2 = num2;
+            OperationSign = operationSign;
         }
 
-        public override void printCoordinates()
+        public void DoOperation()
         {
-            Console.WriteLine("The coordinates of the point is ("+_x+","+_y+","+_z+").");
+            double result = 0;
+            switch (OperationSign)
+            {
+               case '+':
+                   result = Num1 + Num2;
+                   Console.WriteLine(Num1 +" + "+Num2+" = "+result);
+                   break;
+               case '-':
+                   result = Num1 - Num2;
+                   Console.WriteLine(Num1 +" - "+Num2+" = "+result);
+                   break;
+               case '*':
+                   result = Num1 * Num2;
+                   Console.WriteLine(Num1 +" * "+Num2+" = "+result);
+                   break;
+               case '/':
+                   try
+                   {
+                       result = (double)Num1 / Num2;
+                       Console.WriteLine(Num1 + " / " + Num2 + " = " + result);
+                   }
+                   catch (DivideByZeroException x)
+                   {
+                       Console.WriteLine("Second Number cannot be a 0 !");
+                   }
+                   break;
+               default:
+                   Console.WriteLine("Error..");
+                   break;
+            }
         }
+        
     }
     
     internal class Program
     {
         public static void Main(string[] args)
         {
-            Point2D point1 = new Point2D();
-            point1.printCoordinates();
-
-            Point2D point2 = new Point2D(3,4);
-            point2.printCoordinates();
-
-            Point3D point3 = new Point3D(3,4,5);
-            point3.printCoordinates();
-            
-            InfoClass.printInfo(point1);
-            InfoClass.printInfo(point2);
-            InfoClass.printInfo(point3);
+            Operation op1 = new Operation(5,2,'/');
+            op1.DoOperation();
         }
     }
 }
