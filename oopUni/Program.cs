b6226fa531b0a520c732052f4732ea9ec545fa40
MousaZa <mous.zeydan@gmail.com>
2024-04-26 10:28:45 +0300

    setters & getters

--- oopUni/Program.cs
+++ oopUni/Program.cs
@@ -2,69 +2,34 @@
 
 namespace oopUni
 {
-
-    internal class Operation
+    internal class Class
     {
-        public int Num1 { set; get; }
-        public int Num2 { set; get; }
-        public char OperationSign { set; get; }
-
-        public Operation()
-        {
-            Num1 = 0;
-            Num2 = 0;
-            OperationSign = '+';
-        }
-
-        public Operation(int num1 , int num2 , char operationSign)
-        {
-            Num1 = num1;
-            Num2 = num2;
-            OperationSign = operationSign;
-        }
-
-        public void DoOperation()
-        {
-            double result = 0;
-            switch (OperationSign)
-            {
-               case '+':
-                   result = Num1 + Num2;
-                   Console.WriteLine(Num1 +" + "+Num2+" = "+result);
-                   break;
-               case '-':
-                   result = Num1 - Num2;
-                   Console.WriteLine(Num1 +" - "+Num2+" = "+result);
-                   break;
-               case '*':
-                   result = Num1 * Num2;
-                   Console.WriteLine(Num1 +" * "+Num2+" = "+result);
-                   break;
-               case '/':
-                   try
-                   {
-                       result = (double)Num1 / Num2;
-                       Console.WriteLine(Num1 + " / " + Num2 + " = " + result);
-                   }
-                   catch (DivideByZeroException x)
-                   {
-                       Console.WriteLine("Second Number cannot be a 0 !");
-                   }
-                   break;
-               default:
-                   Console.WriteLine("Error..");
-                   break;
-            }
-        }
+        private int Member; // field
         
+        public int member  // setter & getter ~ Simpler way ~
+        {
+            set => Member = value;
+            get => Member;
+        }
+
+        public void setMember(int _Member) // setter 
+        {
+            Member = _Member;
+        }
+
+        public int getMember() // getter 
+        {
+            return Member;
+        }
     }
     
     internal class Program
     {
         public static void Main(string[] args)
         {
-            Operation op1 = new Operation(5,2,'/');
-            op1.DoOperation();
+      
+            
+            
         }
     }
 }
