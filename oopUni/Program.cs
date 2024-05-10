244fcab6cfd53b347317968887771adbcf45bd8b
MousaZa <mous.zeydan@gmail.com>
2024-05-10 14:56:48 +0300

    stack queue arrayList

--- oopUni/Program.cs
+++ oopUni/Program.cs
@@ -1,47 +1,43 @@
 ﻿using System;
-using System.Runtime.Remoting.Contexts;
-
+using System.Collections ;
 namespace oopUni
 {
-
-    internal class Complex
-    {
-        private int Real;
-        private int Imaginary;
-        
-        public int real { set => Real = value; get => Real; }
-        public int imaginary { set => Imaginary = value; get => Imaginary; }
-        
-        public Complex(){}
-
-        public Complex(int _real, int _imaginary)
-        {
-            Real = _real;
-            Imaginary = _imaginary;
-        }
-
-        public void PrintNumber()
-        {
-            Console.WriteLine(Real + " + " + Imaginary + "i");
-        }
-
-        public static Complex operator +(Complex complex1, Complex complex2)
-        {
-            Complex result = new Complex();
-            result.Real = complex1.Real + complex2.Real;
-            result.Imaginary = complex1.Imaginary + complex2.Imaginary;
-            return result;
-        }
-    }
-   
     internal class Program
     {
         public static void Main(string[] args)
         {
-            Complex number1 = new Complex(3, 4);
-            Complex number2 = new Complex(4, 5);
-            Complex number3 = number1 + number2;
-            number3.PrintNumber(); 
+            ArrayList List = new ArrayList();
+            List.Add(5);
+            List.Add(1.0f);
+            List.Add("Hello");
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            stack.Pop();
+            Queue queue = new Queue();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Dequeue();
+            foreach (var var in queue)
+            {
+                Console.WriteLine(var);
+            }
+            Console.WriteLine("==============");
+            foreach (var var in stack)
+            {
+                Console.WriteLine(var);
+            }
+            Console.WriteLine("==============");
+            foreach (var var in List)
+            {
+                Console.WriteLine(var);
+            }
         }
     }
 }
